ab21f0e259595378a628bda3fb9481f7abcf8357
ganeshreddy09 <ganeshreddy87908@gmail.com>
2026-03-10 09:33:17 +0530

    Initial commit - UC1 Hotel Booking Application

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+
+public class BookMyStayApp {
+
+
+    public static void main(String[] args) {
+        System.out.println("   HOTEL BOOKING MANAGEMENT SYSTEM   ");
+        System.out.println("Welcome to the Hotel Booking Application!");
+        System.out.println("System is starting...");
+        System.out.println("Application started successfully.");
+
+
+    }
+}
