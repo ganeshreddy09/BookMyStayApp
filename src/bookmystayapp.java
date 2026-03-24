02fa3e2b3dd6aa3eaa436aa56637775d627c7d0d
ganeshreddy09 <ganeshreddy87908@gmail.com>
2026-03-25 00:55:11 +0530

    UC6:Reservation Confirmation & Room Allocation

--- src/bookmystayapp.java
+++ src/bookmystayapp.java
@@ -1,2 +1,76 @@
-public class bookmystayapp {
+import java.util.*;
+
+class BookingRequest {
+    String guestName;
+    String roomType;
+
+    BookingRequest(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+}
+
+class BookStayApp{
+
+    // Queue for FIFO booking processing
+    private Queue<BookingRequest> bookingQueue = new LinkedList<>();
+
+    // Set to ensure unique room IDs
+    private Set<String> allocatedRooms = new HashSet<>();
+
+    // Map room type -> allocated room IDs
+    private Map<String, Set<String>> roomTypeMap = new HashMap<>();
+
+    // Track room numbers per type
+    private Map<String, Integer> roomCounter = new HashMap<>();
+
+    public void addBooking(String guest, String roomType) {
+        bookingQueue.add(new BookingRequest(guest, roomType));
+    }
+
+    private String generateRoomId(String roomType) {
+
+        int count = roomCounter.getOrDefault(roomType, 0) + 1;
+        roomCounter.put(roomType, count);
+
+        String roomId = roomType + "-" + count;
+
+        allocatedRooms.add(roomId);
+
+        roomTypeMap
+                .computeIfAbsent(roomType, k -> new HashSet<>())
+                .add(roomId);
+
+        return roomId;
+    }
+
+    public void processBookings() {
+
+        System.out.println("Room Allocation Processing");
+
+        while (!bookingQueue.isEmpty()) {
+
+            BookingRequest request = bookingQueue.poll();
+
+            String roomId = generateRoomId(request.roomType);
+
+            System.out.println(
+                    "Booking confirmed for Guest: " +
+                            request.guestName +
+                            ", Room ID: " +
+                            roomId
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BookStayApp service = new BookStayApp();
+
+        service.addBooking("Abhi", "Single");
+        service.addBooking("Shlok", "Single");
+        service.addBooking("Vani", "Suite");
+
+        service.processBookings();
+    }
 }
